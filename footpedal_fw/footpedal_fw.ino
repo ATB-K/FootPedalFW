99607747c9ba2f3a5985d45b506a078d390f2099
ATB-K <atbkeita.developer@gmail.com>
2021-09-03 00:04:01 +0900

    FW追加

---
+++ footpedal_fw/footpedal_fw.ino
@@ -0,0 +1,78 @@
+#include <Mouse.h>
+#include <Keyboard.h>
+
+// キー割り当て
+const int BTN_1 = 0x80;
+const int BTN_2 = MOUSE_RIGHT;
+const int BTN_3 = MOUSE_LEFT;
+
+// 定数
+const int BTN_PUSH    = 0;
+const int BTN_RELEASE = 1;
+const int PIN[] = {8, 9, 10};                  // 利用PIN定義
+const int BTN_NUM  = sizeof(PIN) / sizeof(int);// ボタンの数
+
+// ペダルごとの動作定義
+static void push_btn_1(void){ Keyboard.press( BTN_1 ); }
+static void push_btn_2(void){ Mouse.press( BTN_2 ); }
+static void push_btn_3(void){ Mouse.press( BTN_3 ); }
+static void release_btn_1(void){ Keyboard.release( BTN_1 ); }
+static void release_btn_2(void){ Mouse.release( BTN_2 ); }
+static void release_btn_3(void){ Mouse.release( BTN_3 ); }
+
+void (* const FUNC_BTNS[BTN_NUM][2])(void) = {
+    {push_btn_1, release_btn_1},
+    {push_btn_2, release_btn_2},
+    {push_btn_3, release_btn_3}
+};
+
+// グローバル変数
+int BEFORE_VALUE[BTN_NUM] = {HIGH, HIGH, HIGH};  // 押下情報記録用 (High -> Low検知式の為、初期値High)
+
+/*
+ * 通電時の初期化処理
+ */
+void setup(){
+  // ピン設定
+  for (int i=0 ; i < sizeof(PIN) / sizeof(int) ; i++) {
+      pinMode( PIN[i], INPUT_PULLUP );
+  }
+}
+
+/*
+ * 通電中のループ処理
+ */
+void loop(){
+  for (int i=0 ; i < BTN_NUM ; i++) {
+
+    // ピン値読み取り
+    int value = digitalRead( PIN[i] );
+
+    if (value == LOW) {
+      if (BEFORE_VALUE[i] == HIGH) {
+        // High -> Lowの場合
+        FUNC_BTNS[i][BTN_PUSH]();
+
+        // ボタンの変化を記録
+        BEFORE_VALUE[i] = value;
+      } else {
+        // Low -> Lowの場合
+        // Do Nothing
+      }
+    } else {
+      if (BEFORE_VALUE[i] == LOW) {
+        // Low -> Highの場合
+        FUNC_BTNS[i][BTN_RELEASE]();
+
+        // ボタンの変化を記録
+        BEFORE_VALUE[i] = value;
+      } else {
+        // High -> Highの場合
+        // Do Nothing
+      }
+    }
+  }
+
+  // ポーリング間隔
+  delay( 10 );
+}
